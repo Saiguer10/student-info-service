e7d7ba9fb747b35a6a4d758efd00a8da68b69dd0
Saiguer10 <eljosemsp@gmail.com>
2025-05-19 16:36:05 +0200

    Commit inicial configurado packages correctos

---
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Student;
+import com.example.demo.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Long id) {
+        return studentRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
